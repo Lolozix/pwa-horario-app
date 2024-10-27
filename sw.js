f67e18e61acf83350817c764ce9511117833bcdb
kaykevital <kayke.macedo@estudante.ifms.edu.br>
2024-10-27 19:05:26 -0400

    Commit de adicições

---
+++ sw.js
@@ -0,0 +1,26 @@
+let cacheName = "my-first-pwa";
+
+let filesToCache = ["/", "/index.html", "/pages/horarioAula.html", "/pages/horarioPE.html", "/pages/pagina" ,"/css/index.css", "/css/horarioAula.css", "/css/horarioPE.css", "/js/main.js"];
+
+/*inicializando a service worker e fazendo o download do conteúdo da aplicação */
+
+self.addEventListener("install", (e) => {
+
+    e.waitUntil(caches.open(cacheName).then(function (cache) { return cache.addAll(filesToCache); }));
+});
+
+/*disponibilizando o conteudo quando estiver offline */
+
+self.addEventListener("fetch", (e) => {
+
+    e.respondWith(
+
+        caches.match(e.request).then((response) => {
+
+            return response || fetch(e.request);
+
+        })
+
+    );
+
+});
